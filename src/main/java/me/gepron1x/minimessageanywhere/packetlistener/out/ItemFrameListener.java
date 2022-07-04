92050f6b24e4ddcdab7cc2985060cd8365e61d25
Георгий Пронюк <georgijpronuk@gmail.com>
2022-07-04 21:23:47 +0300

    Fix item frames

--- src/main/java/me/gepron1x/minimessageanywhere/packetlistener/out/ItemFrameListener.java
+++ src/main/java/me/gepron1x/minimessageanywhere/packetlistener/out/ItemFrameListener.java
@@ -3,36 +3,28 @@ package me.gepron1x.minimessageanywhere.packetlistener.out;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.BukkitConverters;
 import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import me.gepron1x.minimessageanywhere.handler.ComponentHandler;
 import me.gepron1x.minimessageanywhere.packetlistener.out.item.ConvertedItemStack;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
-
-import java.util.Optional;
 
 public final class ItemFrameListener extends AbstractListener {
 
-    private final WrappedDataWatcher.WrappedDataWatcherObject itemFrameIcon;
 
     public ItemFrameListener(Plugin plugin, ComponentHandler handler) {
         super(plugin, handler, PacketType.Play.Server.ENTITY_METADATA);
-        this.itemFrameIcon = new WrappedDataWatcher.WrappedDataWatcherObject(8, WrappedDataWatcher.Registry.getItemStackSerializer(true));
     }
 
     @Override
     public void onPacketSending(PacketEvent event) {
         final PacketContainer packet = event.getPacket();
         final WrappedDataWatcher wdw = new WrappedDataWatcher(packet.getWatchableCollectionModifier().read(0));
-        Object item = wdw.getObject(itemFrameIcon);
-        if (item == null) return;
-        wdw.setObject(itemFrameIcon, ((Optional<?>) item)
-                .map(BukkitConverters.getItemStackConverter()::getSpecific)
-                .map(i -> {
-                    new ConvertedItemStack(this.handler, event.getPlayer(), i).convert();
-                    return i;
-                }).map(BukkitConverters.getItemStackConverter()::getGeneric)
-        );
+        if (!(wdw.getObject(8) instanceof ItemStack itemStack)) {
+            return;
+        }
+        new ConvertedItemStack(this.handler, event.getPlayer(), itemStack).convert();
+        wdw.setObject(8, itemStack);
 
     }
 }
