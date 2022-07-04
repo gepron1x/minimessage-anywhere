21bd1cd2302ab61664552fc2d112bbe374a48ff3
Георгий Пронюк <georgijpronuk@gmail.com>
2022-07-04 20:08:24 +0300

    fix item frame listener init

--- src/main/java/me/gepron1x/minimessageanywhere/packetlistener/out/ItemFrameListener.java
+++ src/main/java/me/gepron1x/minimessageanywhere/packetlistener/out/ItemFrameListener.java
@@ -13,19 +13,20 @@ import java.util.Optional;
 
 public final class ItemFrameListener extends AbstractListener {
 
-    private static final WrappedDataWatcher.WrappedDataWatcherObject ITEM_FRAME_ICON = new WrappedDataWatcher.WrappedDataWatcherObject(8, WrappedDataWatcher.Registry.getItemStackSerializer(true));
+    private final WrappedDataWatcher.WrappedDataWatcherObject itemFrameIcon;
 
     public ItemFrameListener(Plugin plugin, ComponentHandler handler) {
         super(plugin, handler, PacketType.Play.Server.ENTITY_METADATA);
+        this.itemFrameIcon = new WrappedDataWatcher.WrappedDataWatcherObject(8, WrappedDataWatcher.Registry.getItemStackSerializer(true));
     }
 
     @Override
     public void onPacketSending(PacketEvent event) {
         final PacketContainer packet = event.getPacket();
         final WrappedDataWatcher wdw = new WrappedDataWatcher(packet.getWatchableCollectionModifier().read(0));
-        Object item = wdw.getObject(ITEM_FRAME_ICON);
+        Object item = wdw.getObject(itemFrameIcon);
         if (item == null) return;
-        wdw.setObject(ITEM_FRAME_ICON, ((Optional<?>) item)
+        wdw.setObject(itemFrameIcon, ((Optional<?>) item)
                 .map(BukkitConverters.getItemStackConverter()::getSpecific)
                 .map(i -> {
                     new ConvertedItemStack(this.handler, event.getPlayer(), i).convert();
